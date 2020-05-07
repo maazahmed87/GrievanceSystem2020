6a693228e48a65c02efd808b6663b4005abadc61
Maaz Ahmed <elfilosof@protonmail.com>
2020-05-07 14:34:09 +0530

    Changes to Navabar and addittional

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
-import React, { Fragment } from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
 import firebase from "../firebase";
 import Ticket from "../SidePanel/Ticket";
+import $ from "jquery";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-import { Container } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
 
 class Main extends React.Component {
   state = {
@@ -15,52 +18,106 @@ class Main extends React.Component {
       .signOut()
       .then(() => console.log("signed out!"));
   };
+  componentDidMount() {
+    $("#menu-toggle").click(function (e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  }
 
   render() {
     const { currentUser } = this.props;
     const { active } = this.state;
 
     return (
-      <Fragment>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Navbar.Brand href="/">Grievance System</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav>
-              <NavDropdown
-                title={currentUser.displayName}
-                id="collasible-nav-dropdown"
-              >
-                <NavDropdown.Item href="#action/3.1">
-                  Signed in as{" "}
-                  <strong style={{ textTransform: "capitalize" }}>
-                    {currentUser.displayName}
-                  </strong>
-                </NavDropdown.Item>
-                <NavDropdown.Item>Change Avatar</NavDropdown.Item>
-                <NavDropdown.Item>Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item
-                  href="#action/3.4"
-                  onClick={this.handleSignout}
-                >
-                  Sign out
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link onClick={() => this.setState({ active: "account" })}>
-                Account
-              </Nav.Link>
-              <Nav.Link onClick={() => this.setState({ active: "tickets" })}>
-                Tickets
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Container fluid>
-          {active === "account" && <h2>Account Section</h2>}
-          {active === "tickets" && <Ticket currentUser={currentUser} />}
-        </Container>
-      </Fragment>
+      <div className="d-flex" id="wrapper">
+        <div className="bg-light border-right" id="sidebar-wrapper">
+          <div className="sidebar-heading">Grivance System </div>
+          <div className="list-group list-group-flush">
+            <a className="list-group-item list-group-item-action bg-light">
+              Dashboard
+            </a>
+            <a
+              className="list-group-item list-group-item-action bg-light"
+              onClick={() => this.setState({ active: "tickets" })}
+            >
+              Tickets
+            </a>
+            <a
+              className="list-group-item list-group-item-action bg-light"
+              onClick={() => this.setState({ active: "account" })}
+            >
+              Account
+            </a>
+          </div>
+        </div>
+
+        <div id="page-content-wrapper">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <button className="btn" id="menu-toggle">
+              <Icon name="sidebar" />
+            </button>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <img
+                      src={currentUser.photoURL}
+                      alt="user"
+                      style={{ height: " 35px", borderRadius: "50%" }}
+                    />
+                  </a>
+                  <div
+                    className="dropdown-menu dropdown-menu-right"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <a className="dropdown-item" href="#">
+                      Signed in as <strong>{currentUser.displayName}</strong>
+                    </a>
+                    <div className="dropdown-divider"></div>
+                    <a
+                      className="dropdown-item"
+                      href="#"
+                      onClick={this.handleSignout}
+                    >
+                      Logout
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <div className="container-fluid">
+            {active === "account" && <h2>Account Section</h2>}
+            {active === "tickets" && <Ticket currentUser={currentUser} />}
+          </div>
+        </div>
+      </div>
     );
   }
 }
